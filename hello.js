de5692e854e5ecadab2fb8f85c42296d2b27ddf3
rom <mozzieisme@gmail.com>
2021-09-20 15:23:09 -0400

    Difference between return and console.log

--- hello.js
+++ hello.js
@@ -1,11 +1,47 @@
 
 
-const sayHello  = function (name) {
-  console.log("Hello, " + name);
+// const sayHello  = function (name) {
+//   console.log("Hello, " + name);
+// }
+
+// sayHello();
+
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+
+
+
+// console.log:
+// const sayHelloToConsole  = function (name) {
+//   console.log("Hello, " + name);
+// }
+// sayHelloToConsole('John'); 
+
+
+
+// return:
+// const returnSayHello  = function (name) {
+//   return "Hello, " + name;
+// }
+// const greeting = returnSayHello('John');
+
+
+
+
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
 }
+const greeting = returnSayHello('John');
+console.log(greeting);
 
-sayHello();
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+
+
+
+
+
+
